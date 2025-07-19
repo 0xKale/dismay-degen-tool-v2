e226fa7bb91a458e284f48f163e3545ba76e787a
Kale <93299449+0xKale@users.noreply.github.com>
2025-07-20 01:02:05 +1000

    Add project files.

---
+++ GRIIM DX9 IMGUI/src/functions.h
@@ -0,0 +1,76 @@
+#include <windows.h>
+#include <shellapi.h>
+#pragma once
+
+enum heads {
+    HEAD_1, // host
+    HEAD_2, // friends
+    HEAD_3, // kick tool
+    HEAD_4, // ban tool
+    HEAD_5, // console
+    HEAD_6  // misc
+};
+// Memory Locations
+
+
+namespace functions
+{
+    // Initial Values
+    extern heads head_selected = HEAD_1;
+    extern int combobox = 0;
+    //extern bool checkbox[7] = { false, false, false , false, false , false, false };
+    extern int fFPS = 125;
+    extern float fFOV = 65.0f;
+	//extern const char* combobox_maps[27] = { "Afghan","Derail","Estate","Favela","Highrise","Invasion","Karachi","Quarry","Rundown","Rust","Scrapyard","Skidrow","Sub Base","Terminal","Underpass","Wasteland","-----DLC MAPS-----","Bailout","Crash","Salvage","Overgrown","Storm","Carnival","Fuel","Strike","Trailer Park","Vacant" };
+    extern bool bChat = true; // Killcam checkbox, 0 for off, 1 for on
+
+	// Memory Addresses
+	int mFOV = 0xAAC1F8; // FOV
+    int mFPS = 0x1B90730; // FPS
+    int mChat = 0xAA61C0; // Chat
+
+
+	// Handle Mouse Cursor
+    inline void handleMouseCursor()
+    {
+        if (gui::open)
+        {
+            ImGuiIO& io = ImGui::GetIO();
+            io.MouseDrawCursor = true;
+            *reinterpret_cast<int**>(0x6427D3D) = nullptr; // Unhook mouse
+        }
+        else
+        {
+            ImGuiIO& io = ImGui::GetIO();
+            io.MouseDrawCursor = false;
+            *reinterpret_cast<int*>(0x6427D3D) = 1; // Release mouse
+        }
+    }
+
+    void funFOV(){
+        DWORD dwPointer = *(DWORD*)mFOV; // FOV
+        *(float*)(dwPointer + 0xC) = fFOV; // Use fully qualified name
+    }
+
+	void funFPS()
+	{
+		DWORD dwPointer = *(DWORD*)mFPS; // FPS
+		*(int*)(dwPointer + 0xC) = fFPS; // Set FPS
+	}
+
+	void funChat()
+	{
+		DWORD dwPointer = *(DWORD*)mChat; // Chat
+		if (bChat == true)
+		{
+			*(int*)(dwPointer + 0xC) = 72000; // Enable Chat
+		}
+		else
+		{
+			*(int*)(dwPointer + 0xC) = 0; // Disable Chat
+		}
+	}
+
+
+
+}
---
+++ GRIIM DX9 IMGUI/src/gui.cpp
@@ -0,0 +1,341 @@
+#include "gui.h"
+#include "hooks.h"
+#include "../ext/imgui/imgui.h"
+#include "../ext/imgui/imgui_impl_win32.h"
+#include "../ext/imgui/imgui_impl_dx9.h"
+
+#include <stdexcept>
+#include <string>
+#include <iostream>
+#include <iostream>
+#include <map>
+
+#include "byte.h"
+#include "elements.h"
+#include "functions.h"
+
+
+
+namespace fonts {
+	ImFont* medium;
+	ImFont* semibold;
+	ImFont* logo;
+	ImFont* clarityfont;
+	ImFont* PTRootUIMedium;
+	ImFont* PTRootUIBold;
+}
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(
+	HWND window,
+	UINT message,
+	WPARAM wideParam,
+	LPARAM longParam
+);
+
+// window process
+LRESULT CALLBACK WindowProcess(
+	HWND window,
+	UINT message,
+	WPARAM wideParam,
+	LPARAM longParam
+);
+
+bool gui::SetupWindowClass(const char* windowClassName) noexcept
+{
+	//Populate Window Class
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = DefWindowProc;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = GetModuleHandle(NULL);
+	windowClass.hIcon = NULL;
+	windowClass.hCursor = NULL;
+	windowClass.hbrBackground = NULL;
+	windowClass.lpszMenuName = NULL;
+	windowClass.lpszClassName = windowClassName;
+	windowClass.hIconSm = NULL;
+
+	// register
+	if (!RegisterClassEx(&windowClass))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void gui::DestroyWindowClass() noexcept
+{
+	UnregisterClass(
+		windowClass.lpszClassName,
+		windowClass.hInstance
+	);
+}
+
+bool gui::SetupWindow(const char* windowName) noexcept
+{
+	// Setup Temp Window
+	window = CreateWindow(
+		windowClass.lpszClassName,
+		windowName,
+		WS_OVERLAPPEDWINDOW,
+		0,
+		0,
+		100,
+		100,
+		0,
+		0,
+		windowClass.hInstance,
+		0
+	);
+	if (!window) {
+		return false;
+	}
+	return true;
+}
+void gui::DestroyWindow() noexcept
+{
+	if (window) {
+		DestroyWindow(window);
+	}
+}
+
+bool gui::SetupDirectX() noexcept
+{
+	const auto handle = GetModuleHandle("d3d9.dll");
+
+	if (!handle)
+	{
+		return false;
+	}
+
+	using CreateFn = LPDIRECT3D9(__stdcall*)(UINT);
+
+	const auto create = reinterpret_cast<CreateFn>(GetProcAddress(handle, "Direct3DCreate9"));
+
+	if (!create) 
+	{
+		return false;
+	}
+
+	d3d9 = create(D3D_SDK_VERSION);
+
+	if (!d3d9)
+	{
+		return false;
+	}
+
+	D3DPRESENT_PARAMETERS params = { };
+	params.BackBufferWidth = 0;
+	params.BackBufferHeight = 0;
+	params.BackBufferFormat = D3DFMT_UNKNOWN;
+	params.BackBufferCount = 0;
+	params.MultiSampleType = D3DMULTISAMPLE_NONE;
+	params.MultiSampleQuality = NULL;
+	params.hDeviceWindow = window;
+	params.Windowed = 1;
+	params.EnableAutoDepthStencil = 0;
+	params.AutoDepthStencilFormat = D3DFMT_UNKNOWN;
+	params.Flags = NULL;
+	params.FullScreen_RefreshRateInHz = 0;
+	params.PresentationInterval = 0;
+
+	if (d3d9->CreateDevice(
+		D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_NULLREF,
+		window,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING | D3DCREATE_DISABLE_DRIVER_MANAGEMENT,
+		&params,
+		&device) < 0) return false;
+
+	return true;
+
+}
+void gui::DestroyDirectX() noexcept
+{
+	if (device)
+	{
+		device->Release();
+		device = NULL;
+	}
+	if (d3d9)
+	{
+		d3d9->Release();
+		d3d9 = NULL;
+	}
+}
+
+// Setup Device
+void gui::Setup()
+{
+	if (!SetupWindowClass("hackClass001"))
+		throw std::runtime_error("Failed to create window class.");
+
+	if (!SetupWindow("Hack Window"))
+		throw std::runtime_error("Failed to create window.");
+
+	if (!SetupDirectX())
+		throw std::runtime_error("Failed to setup DirectX.");
+
+	DestroyWindow();
+	DestroyWindowClass();
+}
+
+void gui::SetupMenu(LPDIRECT3DDEVICE9 device) noexcept
+{
+	auto params = D3DDEVICE_CREATION_PARAMETERS{};
+	device->GetCreationParameters(&params);
+
+	window = params.hFocusWindow;
+
+	originalWindowProcess = reinterpret_cast<WNDPROC>(
+		SetWindowLongPtr(window, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WindowProcess))
+	);
+
+	//Imgui
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(device);
+
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	// Load Fonts
+	ImFontConfig font_config;
+	font_config.PixelSnapH = false;
+	font_config.OversampleH = 5;
+	font_config.OversampleV = 5;
+	font_config.RasterizerMultiply = 1.2f;
+
+	static const ImWchar ranges[] =
+	{
+		0x0020, 0x00FF, // Basic Latin + Latin Supplement
+		0x0400, 0x052F, // Cyrillic + Cyrillic Supplement
+		0x2DE0, 0x2DFF, // Cyrillic Extended-A
+		0xA640, 0xA69F, // Cyrillic Extended-B
+		0xE000, 0xE226, // icons
+		0,
+	};
+
+	font_config.GlyphRanges = ranges;
+
+	fonts::medium = io.Fonts->AddFontFromMemoryTTF(PTRootUIMedium, sizeof(PTRootUIMedium), 15.0f, &font_config, ranges);
+	fonts::semibold = io.Fonts->AddFontFromMemoryTTF(PTRootUIBold, sizeof(PTRootUIBold), 17.0f, &font_config, ranges);
+
+	fonts::logo = io.Fonts->AddFontFromMemoryTTF(catrine_logo, sizeof(catrine_logo), 17.0f, &font_config, ranges);
+
+	// Our state
+	ImVec4 clear_color = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+
+	setup = true;
+}
+
+void gui::Destroy() noexcept
+{
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	// restore wnd proc
+	SetWindowLongPtr(
+		window,
+		GWLP_WNDPROC,
+		reinterpret_cast<LONG_PTR>(originalWindowProcess)
+	);
+
+	DestroyDirectX();
+}
+
+void gui::Render() noexcept
+{
+
+	//functions::head_selected = HEAD_1;
+	//functions::checkbox[0];
+	//functions::combobox, functions::IFPS = 0;
+	//functions::combobox_maps[27];
+
+	ImGui::SetNextWindowSize({ 500, 370 });
+
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+
+	ImGui::Begin("dismay's private degen tool", nullptr, ImGuiWindowFlags_NoDecoration);
+	{
+		auto draw = ImGui::GetWindowDrawList();
+
+		auto pos = ImGui::GetWindowPos();
+		auto size = ImGui::GetWindowSize();
+
+		draw->AddRectFilled(pos, ImVec2(pos.x + size.x, pos.y + 51), ImColor(24, 24, 24), 9.0f, ImDrawFlags_RoundCornersTop);
+		draw->AddRectFilledMultiColorRounded(pos, ImVec2(pos.x + 55, pos.y + 51), ImColor(1.0f, 1.0f, 1.0f, 0.00f), ImColor(1.0f, 1.0f, 1.0f, 0.05f), ImColor(1.0f, 1.0f, 1.0f, 0.00f), ImColor(1.0f, 1.0f, 1.0f, 0.00f), ImColor(1.0f, 1.0f, 1.0f, 0.05f), 9.0f, ImDrawFlags_RoundCornersTopLeft);
+
+		//draw->AddText(fonts::logo, 17.0f, ImVec2(pos.x + 25, pos.y + 17), ImColor(192, 203, 229), "logo");
+		draw->AddText(fonts::clarityfont, 17.0f, ImVec2(pos.x + 20, pos.y + 18), ImColor(192, 203, 229), "dismay's private");
+
+		ImGui::SetCursorPos({ 125, 19 });
+		ImGui::BeginGroup(); {
+			if (elements::tab("Host", functions::head_selected == HEAD_1)) functions::head_selected = HEAD_1;
+			ImGui::SameLine();
+			if (elements::tab("Friends", functions::head_selected == HEAD_2)) functions::head_selected = HEAD_2;
+		}
+		ImGui::EndGroup();
+
+		switch (functions::head_selected) {
+		case HEAD_1:
+			draw->AddText(fonts::medium, 14.0f, ImVec2(pos.x + 25, pos.y + 60), ImColor(1.0f, 1.0f, 1.0f, 0.6f), "Tab header 1");
+
+			ImGui::SetCursorPos({ 25, 85 });
+			ImGui::BeginChild("##container", ImVec2(190, 275), false, ImGuiWindowFlags_NoScrollbar); {
+				ImGui::SliderFloat("FOV", &functions::fFOV, 65.0f, 120.0f); // FOV Slider
+				ImGui::SliderInt("FPS", &functions::fFPS, 60, 500); // FPS Slider
+				ImGui::Checkbox("Enable/Disable Chat", &functions::bChat); // Chat Enable/Disable Checkbox
+
+			}
+			ImGui::EndChild();
+			draw->AddText(fonts::medium, 14.0f, ImVec2(pos.x + 285, pos.y + 60), ImColor(1.0f, 1.0f, 1.0f, 0.6f), "Tab header 2"); // header text
+			ImGui::SetCursorPos({ 285, 85 });
+
+			ImGui::BeginChild("##container1", ImVec2(190, 275), false, ImGuiWindowFlags_NoScrollbar); 
+			{
+
+			}
+			ImGui::EndChild();
+			break;
+		}
+	}
+	ImGui::End();
+}
+
+LRESULT CALLBACK WindowProcess(
+	HWND window,
+	UINT message,
+	WPARAM wideParam,
+	LPARAM longParam
+)
+{
+	// toggle menu
+	if (GetAsyncKeyState(VK_HOME) & 1) {
+		gui::open = !gui::open;
+	}
+	functions::handleMouseCursor(); // release mouse
+	functions::funFOV(); // FOV
+	functions::funChat(); // Chat Enable/Disable
+	functions::funFPS(); // FPS
+
+	// Pass Messages to Imgui
+	if (gui::open && ImGui_ImplWin32_WndProcHandler(
+		window,
+		message,
+		wideParam,
+		longParam
+	)) return 1L;
+
+	return CallWindowProc(
+		gui::originalWindowProcess,
+		window,
+		message,
+		wideParam,
+		longParam
+	);
+}
